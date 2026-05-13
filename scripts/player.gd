253b5426555c455ffe88c62e3c4a51056ff08931
Roha Pathan <rohaabropathan@gmail.com>
2026-05-13 14:57:23 +0500

    updated player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+ 
+# speed in pixels/sec
+var speed = 500 
+ 
+func _physics_process(_delta):
+	# setup direction of movement
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# stop diagonal movement by listening for input then setting axis to zero
+	if Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left"):
+		direction.y = 0
+	elif Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down"):
+		direction.x = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		direction = Vector2.ZERO
+	
+	#normalize the directional movement
+	direction = direction.normalized()
+	# setup the actual movement
+	velocity = (direction * speed)
 	move_and_slide()
